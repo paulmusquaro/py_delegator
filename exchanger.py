ab1e4f5fb3f5d5bf0ff20494a6d2a16a34ee4756
Serhiyko <magdisyuk1999@gmail.com>
2023-09-09 13:50:34 +0300

    new exchanger

--- exchanger.py
+++ exchanger.py
@@ -2,34 +2,49 @@ import requests
 from datetime import datetime
 
 
-api_key = '4c4e8fdc5f8b4a1b8c417b629d43d241'
+class CurrencyConverter:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.url = 'https://openexchangerates.org/api/latest.json'
+
+    def convert_currency(self, user_input):
+        response = requests.get(self.url, params={'app_id': self.api_key, 'base': 'USD'})
+
+        if response.status_code == 200:
+            data = response.json()
+            current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            if user_input == 'EXIT':
+                return "До побачення!"
+
+            if user_input in data['rates']:
+                rate = data['rates'][user_input]
+                return f"Курс {user_input} станом на {current_datetime}: {rate}"
+            else:
+                return f"Даних для валюти {user_input} немає в наявності"
+        else:
+            return f"Помилка при отриманні даних. Статус код: {response.status_code}"
 
 
-url = f'https://openexchangerates.org/api/latest.json'
+def ex_main():
+    api_key = '4c4e8fdc5f8b4a1b8c417b629d43d241'
+    converter = CurrencyConverter(api_key)
 
-while True:
-
-    response = requests.post(url, params={'app_id': api_key, 'base': 'USD'})
-
-
-    if response.status_code == 200:
-        data = response.json()
-
-
-        current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-
-        user_input = input(f"Введіть код валюти (наприклад, EUR або GBP), або 'Exit' для виходу: ").strip().upper()
-
-
-        if user_input == 'EXIT':
+    while True:
+        user_input = input("Введіть код валюти (наприклад, EUR або GBP), або 'EXIT' для виходу: ").strip().upper()
+        result = converter.convert_currency(user_input)
+        print(result)
+        if result == "До побачення!":
             break
 
 
-        if user_input in data['rates']:
-            rate = data['rates'][user_input]
-            print(f"Курс {user_input} станом на {current_datetime}: {rate}")
-        else:
-            print(f"Даних для валюти {user_input} немає в наявності")
-    else:
-        print(f"Помилка при отриманні даних. Статус код: {response.status_code}")
+
+
+
+
+
+
+
+
+
+
