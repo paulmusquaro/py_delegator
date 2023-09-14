d0c1f49120fc5d7480ecba81bcbc7b61a411baa3
paulmusquaro <136118097+paulmusquaro@users.noreply.github.com>
2023-09-14 15:20:17 +0300

    final corrections nine

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # py_delegator
-First Team Progect
+*First Team Project*
+
+This repository contains a set of console utilities that you can use for various tasks such as note creation, file organization, currency conversion, calendar management, and many more. Each utility has its own instruction file and detailed description below.
+
+## Utilities
+
+1. **Calendar** (`calend.py`)
+   - Use this utility to manage your calendar and events.
+   - Run it by executing `calend_main()`.
+
+2. **Address Book** (`Addressbook.py`)
+   - A utility for storing and managing contacts.
+   - Run it by executing `start()`.
+
+3. **Notebook** (`notebook/nb_main.py`)
+   - Create, search, and edit notes.
+   - Run it by executing `nb_main()`.
+
+4. **File Organizer** (`file_sorter.py`)
+   - Organize files by extensions and other criteria.
+   - Run it by executing `start()`.
+
+5. **Currency Converter** (`exchanger.py`)
+   - Currency conversion using an API.
+   - Run it by executing `ex_main()`.
+
+## Usage Instructions
+
+1. **Clone the Repository**
+
+   Open a command prompt or terminal and execute the following command to clone the repository:
+    - *git clone https://github.com/paulmusquaro/py_delegator.git*
+
+
+2. **Run the Chosen Utility**
+
+   Navigate to the folder with the desired utility and execute the respective file using Python with the following commands:
+    - *cd py_delegator*
+    - *python calend.py*
+
+
+3. **Follow the Instructions**
+
+   The utilities will provide you with instructions and execution options while working with them.
+
+
+### Installation via pip
+
+   You can also install **py-delegator** using pip as an alternative method. To do this, follow these steps:
+
+1. **Install via pip**
+
+   Open a command prompt or terminal and execute the following command:
+   - *pip install -i https://test.pypi.org/simple/ py-delegator==0.1.5*
+
+2. **Run the Chosen Utility**
+
+   After installation is complete, you can run the desired utility from anywhere in your terminal by simply invoking its command.
+
+
+## Requirements
+
+- Python 3.x
+- Python libraries as specified in each individual utility's file.
+
+## Contribution
+
+If you want to contribute to the development of these utilities or report issues, please open issues or create pull requests.
+
+## License
+
+This project is distributed under the MIT License. For more information, see the [LICENSE](LICENSE) file.
+
--- src/py_delegator/menu.py
+++ src/py_delegator/menu.py
@@ -1,20 +1,20 @@
-import importlib
+# import importlib
 import os
-from calend import calend_main
-from Addressbook import start
-from notebook.nb_main import NoteManager
-from file_sorter import start
-from exchanger import ex_main
+from .calend import calend_main
+from .Addressbook import start as start_ab
+from .notebook.nb_main import NoteManager
+from .file_sorter import start as start_fs
+from .exchanger import ex_main
 
 
 class Menu:
     def __init__(self):
         self.choices = {
-            1: ("calend", "calend_main"),
-            2: ("Addressbook", "start"),
+            1: ("file_sorter", start_fs),
+            2: ("Addressbook", start_ab),
             3: ("notebook.nb_main", "nb_main"),
-            4: ("file_sorter", "start"),
-            5: ("exchanger", "ex_main")
+            4: ("calend", calend_main),
+            5: ("exchanger", ex_main)
         }
 
     def make_decision(self, choice):
@@ -31,16 +31,18 @@ class Menu:
 
         try:
 
-            if choice == 4:
+            if choice == 1:
                 directory_path = input("Enter the path to directory: ")
                 if os.path.exists(directory_path) and os.path.isdir(directory_path):
-                    function = getattr(importlib.import_module(module_name), function_name)
-                    function(directory_path)
+                    # function = getattr(importlib.import_module(module_name), function_name)
+                    # function(directory_path)
+                    function_name(directory_path)
                 else:
                     print("Invalid directory path. Please enter a valid directory path.")
             else:
-                function = getattr(importlib.import_module(module_name), function_name)
-                function()
+                # function = getattr(importlib.import_module(module_name), function_name)
+                # function()
+                function_name()
         except ModuleNotFoundError:
             print("Module not found.")
         except AttributeError:
@@ -48,10 +50,10 @@ class Menu:
 
 def main():
     print("Choose an option:")
-    print("1. Calendar")
+    print("1. File Sorter")
     print("2. Address Book")
     print("3. Notebook")
-    print("4. File Sorter")
+    print("4. Calendar")
     print("5. Exchanger")
     print("6. Exit")
 
@@ -68,5 +70,5 @@ def main():
         except ValueError:
             print("Invalid input. Please enter a number between 1 and 6.")
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
